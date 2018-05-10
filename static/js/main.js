cbe62a4107c7c22f06312987d7ebd867c4d71e13
meeteason <meeteason@gmail.com>
2018-05-10 18:29:38 +0800

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -19,146 +19,205 @@ var dapp_address = "n1xXUSQdCYvP6USG26JrSQo7rM85fSSoC9U";
 
 
 $(function () {
-    
-//   write("sdfadf", 5000, true, function (data) {
-//     if (data.code == 0) {
 
-//       console.log("wirte success,and then get your content...")
-//       //   console.log()
-//       localStorage.setItem("yourAddress", data.data.from)
-//       getYours(data.data.from).then(function (rep) {
-//         console.log("get yours success", rep)
-//       })
 
-//     } else {
-//       console.error(data)
-//     }
-//   })
-  var yourAddress = localStorage.getItem("yourAddress")
-  if (!!yourAddress) {
-    getYours(yourAddress)
-  }
+    //设置时间选择器
+    $('#txtExpire').datepicker({
+        format: "yyyy-mm-dd 23:59:59",
+        language: "zh-CN",
+        clearBtn: true,
+        todayHighlight: true,
+        startDate: dayjs().format("YYYY-MM-DD")
+    });
+    //提交写入
+    $("#saveWrite").click(function () {
+        var date = dayjs($('#txtExpire').val())
+        if (date.isValid()) {
+            var expire = date.diff(dayjs)
+            if (expire > 0) {
+                var cover = $("#coverData").prop("checked")
+                showLoading();
+                write($("#txtContent").val(), expire, cover, function (data) {
+                    var alertSuccess = $("#modalWrite .alert-danger"),
+                        alertError = $("#modalWrite .alert-success")
+                    hideLoading();
+                    if (data.code == 0) {
+                        console.log("wirte success,and then get your content...")
+                        alertSuccess.alert()
+                        localStorage.setItem("yourAddress", data.data.from)
+                        $("#txtAddress").val(data.data.from)
+                        getYours(data.data.from).then(function (rep) {
+                            console.log("get yours success", rep)
+                        })
+
+                    } else {
+                        console.error(data)
+                    }
+                })
+            } else {
+                // The date time wrong
+                //TODO
+            }
+        } else {
+            // TODO
+        }
+    });
+
+    $("#btnGetWrite").click(function () {
+        var _address = $("#txtAddress").val()
+        if (!!_address && _address.length == 35 && _address.startsWith("n1")) {
+            showLoading()
+            getYours(_address).then(function (rep) {
+                hideLoading();
+                var data = JSON.parse(rep.result)
+                showYourCentent(data)
+            })
+        } else {
+            //the address wrong format
+            //TODO
+        }
+    });
+
+    var yourAddress = localStorage.getItem("yourAddress")
+    if (!!yourAddress) {
+        getYours(yourAddress)
+    }
 })
 
 function showYourCentent(data) {
-  if (data.status == 1) {
-    console.log(data.content)
-  } else if (data.status == -1) {
-    console.log("未写入任何信息")
-  } else if (data.status == -2) {
-    console.log("时间未到，不能展示")
-  }
+    if (data.status == 1) {
+        $("#modalFind pre").html(data.content)
+        $("#modalFind").modal("show");
+        console.log(data.content)
+    } else if (data.status == -1) {
+        console.log("未写入任何信息")
+    } else if (data.status == -2) {
+        //设置倒计时
+        $(".count_down").countDown({
+            startTimeStr: dayjs().format("YYYY/MM/DD HH:mm:ss"), //开始时间
+            endTimeStr: dayjs(data.expire).format("YYYY/MM/DD HH:mm:ss"), //结束时间
+            daySelector: ".day_num",
+            hourSelector: ".hour_num",
+            minSelector: ".min_num",
+            secSelector: ".sec_num"
+        });
+
+        $(".count_down").show()
+        console.log("时间未到，不能展示")
+
+    }
 }
 
 function getCount() {
-  return myneb.api.call({
-    from: dapp_address,
-    to: dapp_address,
-    value: 0,
-    contract: {
-      function: "getCount",
-      args: "[]"
-    },
-    gasPrice: 1000000,
-    gasLimit: 2000000,
-  })
+    return myneb.api.call({
+        from: dapp_address,
+        to: dapp_address,
+        value: 0,
+        contract: {
+            function: "getCount",
+            args: "[]"
+        },
+        gasPrice: 1000000,
+        gasLimit: 2000000,
+    })
 }
 
 function getDate() {
-  return myneb.api.call({
-    from: dapp_address,
-    to: dapp_address,
-    value: 0,
-    contract: {
-      function: "getDate",
-      args: "[]"
-    },
-    gasPrice: 1000000,
-    gasLimit: 2000000,
-  });
+    return myneb.api.call({
+        from: dapp_address,
+        to: dapp_address,
+        value: 0,
+        contract: {
+            function: "getDate",
+            args: "[]"
+        },
+        gasPrice: 1000000,
+        gasLimit: 2000000,
+    });
 }
 
 function getYours(yourAddress) {
-  var _from = yourAddress || dapp_address
-  return myneb.api.call({
-    from: _from,
-    to: dapp_address,
-    value: 0,
-    contract: {
-      function: "getYours",
-      args: "[]"
-    },
-    gasPrice: 1000000,
-    gasLimit: 2000000,
-  });
+    var _from = yourAddress || dapp_address
+    return myneb.api.call({
+        from: _from,
+        to: dapp_address,
+        value: 0,
+        contract: {
+            function: "getYours",
+            args: "[]"
+        },
+        gasPrice: 1000000,
+        gasLimit: 2000000,
+    });
 }
 
 function write(content, expire, cover, callback) {
-  var _content = content,
-    _expire = expire || 6000,
-    _cover = cover || true,
-    _call = callback || $.noop;
-  if (!_content && _content.length <= 0) {
-    console.log("内容不能为空")
-    _call.call(this, {
-      code: 1,
-      msg: "内容不能为空"
-    })
-  } else if (_expire < 0) {
-    console.log("过期时间不能为负数")
-    _call.call(this, {
-      code: 1,
-      msg: "过期时间不能为负数"
-    })
-  } else {
+    var _content = content,
+        _expire = expire || 6000,
+        _cover = cover || true,
+        _call = callback || $.noop;
+    if (!_content && _content.length <= 0) {
+        console.log("内容不能为空")
+        _call.call(this, {
+            code: 1,
+            msg: "内容不能为空"
+        })
+    } else if (_expire < 0) {
+        console.log("过期时间不能为负数")
+        _call.call(this, {
+            code: 1,
+            msg: "过期时间不能为负数"
+        })
+    } else {
 
-    var callArgs = JSON.stringify([_content, _expire, _cover]);
+        var callArgs = JSON.stringify([_content, _expire, _cover]);
 
-    var _loopCall = null;
-    var _listener = function (rep) {
-    //   debugger;
-      console.log(rep)
-      if (typeof rep == "string" && rep.indexOf("Error") != -1) {
-        clearTimeout(_loopCall)
-      }
-    }
-
-    var serialNumber = nebPay.call(dapp_address, 0, "Write", callArgs, {
-      qrcode: {
-        showQRCode: true,
-        container: undefined
-      },
-      goods: {
-        name: "wirte",
-        desc: "Wirte something for your futrue"
-      },
-      listener: _listener
-    });
-    // debugger;
-    var _loopFunc = function () {
-      console.log(serialNumber)
-      nebPay.queryPayInfo(serialNumber).then(function (rep) {
-        var data = JSON.parse(rep)
-        if (data.code == 0) {
-          _call.call(this, data)
-        } else {
-          _loopCall = setTimeout(_loopFunc, 1000)
+        var _loopCall = null;
+        var _listener = function (rep) {
+            //   debugger;
+            console.log(rep)
+            if (typeof rep == "string" && rep.indexOf("Error") != -1) {
+                clearTimeout(_loopCall)
+            }
         }
-      }).catch(function (error) {
-        console.error("run write error", error)
+
+        var serialNumber = nebPay.call(dapp_address, 0, "Write", callArgs, {
+            qrcode: {
+                showQRCode: true,
+                container: undefined
+            },
+            goods: {
+                name: "wirte",
+                desc: "Wirte something for your futrue"
+            },
+            listener: _listener
+        });
+        // debugger;
+        var _loopFunc = function () {
+            console.log(serialNumber)
+            nebPay.queryPayInfo(serialNumber).then(function (rep) {
+                var data = JSON.parse(rep)
+                if (data.code == 0) {
+                    _call.call(this, data)
+                } else {
+                    _loopCall = setTimeout(_loopFunc, 1000)
+                }
+            }).catch(function (error) {
+                console.error("run write error", error)
+                _loopCall = setTimeout(_loopFunc, 1000)
+            })
+        }
         _loopCall = setTimeout(_loopFunc, 1000)
-      })
     }
-    _loopCall = setTimeout(_loopFunc, 1000)
-  }
 
 
 }
 
 
-function showLoading(){
+function showLoading() {
     $("#modalLoading").modal("show")
 }
-function showLoading(){
+
+function hideLoading() {
     $("#modalLoading").modal("hide")
 }
