74ac7fe781516c6cd23a252b5923f6052c4c104a
Eason king <yi.biao@outlook.com>
2018-05-10 21:08:25 +0800

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -18,40 +18,77 @@ var dapp_address = "n1xXUSQdCYvP6USG26JrSQo7rM85fSSoC9U";
 // var dapp_address = "n1gPvty9KV4X4EypTuKYT4Q4cBikVacfkpu";
 
 
+// if(typeof(webExtensionWallet) === "undefined") {
+//     alert('星云钱包环境未运行，请安装钱包插件或开启');
+// }else{
+//     console.log('星云钱包环境运行成功');
+// }
+
 $(function () {
 
+    $('#txtExpire').datepicker({
+        format: "yyyy-mm-dd 23:59:59",
+        startDate: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        forceParse: false,
+        todayHighlight: true
+    });
     //提交写入
     $("#saveWrite").click(function () {
-        var date = dayjs($('#txtExpire').val())
-        if (date.isValid()) {
-            var expire = date.diff(dayjs())
-            if (expire > 0) {
-                var cover = $("#coverData").prop("checked")
-                showLoading();
-                write($("#txtContent").val(), expire, cover, function (data) {
-                    var alertSuccess = $("#modalWrite .alert-danger"),
-                        alertError = $("#modalWrite .alert-success")
-                    hideLoading();
-                    if (data.code == 0) {
-                        console.log("wirte success,and then get your content...")
-                        alertSuccess.alert()
-                        localStorage.setItem("yourAddress", data.data.from)
-                        $("#txtAddress").val(data.data.from)
-                        getYours(data.data.from).then(function (rep) {
-                            console.log("get yours success", rep)
-                        })
+        var date = dayjs($('#txtExpire').val()),
+            content = $("#txtContent").val();
+        var alertError = $("#modalWrite .alert-danger"),
+            alertSuccess = $("#modalWrite .alert-success")
 
-                    } else {
-                        console.error(data)
-                    }
-                })
-            } else {
-                // The date time wrong
-                //TODO
-            }
-        } else {
-            // TODO
+        if (!content) {
+            alertError.text("信件内容不能为空！请对自己的未来负责").show();
+            setTimeout(function () {
+                alertError.hide()
+            }, 2000);
+            return
         }
+
+        if (!date.isValid()) {
+            alertError.text("时间格式错误，请重新输入！").show();
+            setTimeout(function () {
+                alertError.hide()
+            }, 2000);
+            return
+        }
+
+        var expire = date.diff(dayjs())
+        if (expire > 0) {
+            var cover = $("#coverData").prop("checked")
+            showLoading();
+            write($("#txtContent").val(), expire, cover, function (data) {
+
+                hideLoading();
+
+                if (data.code == 0) {
+                    console.log("wirte success,and then get your content...")
+                    alertSuccess.text("您的信发送成功！").show()
+                    setTimeout(function () {
+                        alertSuccess.hide()
+                    }, 2000);
+
+                    localStorage.setItem("yourAddress", data.data.from)
+                    $("#txtAddress").val(data.data.from)
+                    getYours(data.data.from).then(function (rep) {
+                        console.log("get yours success", rep)
+                    })
+
+                } else {
+                    console.error(data)
+                }
+            })
+        } else {
+            // The date time wrong
+            //TODO
+            alertError.text("时间格式错误，请重新输入！").show();
+            setTimeout(function () {
+                alertError.hide()
+            }, 2000);
+        }
+
     });
 
     $("#btnGetWrite").click(function () {
@@ -66,6 +103,7 @@ $(function () {
         } else {
             //the address wrong format
             //TODO
+            alert("请输入正确的钱包地址！")
         }
     });
 
@@ -83,6 +121,13 @@ function showYourCentent(data) {
         console.log(data.content)
     } else if (data.status == -1) {
         console.log("未写入任何信息")
+        var letterShow = $("#nolettershow")
+
+        letterShow.show()
+
+        setTimeout(function () {
+            letterShow.hide("slow")
+        }, 3000);
     } else if (data.status == -2) {
         console.log(dayjs().format("YYYY/MM/DD HH:mm:ss"), dayjs(parseInt(data.expire)).format("YYYY/MM/DD HH:mm:ss"))
         //设置倒计时
@@ -176,7 +221,7 @@ function write(content, expire, cover, callback) {
 
         var serialNumber = nebPay.call(dapp_address, 0, "Write", callArgs, {
             qrcode: {
-                showQRCode: true,
+                showQRCode: false,
                 container: undefined
             },
             goods: {
